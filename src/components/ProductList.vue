3ce7f57e19828e8082082b18bc62cec46b17457d
Stefan Djindjic <stefan@Stefans-MacBook-Pro.local>
2020-04-09 14:02:18 +0200

    added shoppingCart

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -7,7 +7,7 @@
         >
         <ul v-else>
             <li v-for="product in products">
-            {{product.title}} - {{product.price}}
+            {{product.title}} - {{product.price | currency}} - {{product.inventory}}
             <button @click="addProductToCart(product)">Add to cart</button>
             </li>
         </ul>
@@ -32,7 +32,7 @@ export default {
         addProductToCart(product) {
             this.$store.dispatch('addProductToCart', product)
         }
-    }
+    },
 
     created () {
         this.loading = true
